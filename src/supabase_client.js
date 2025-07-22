895a0bb83a0cd9923ea25b9b151be23867261e83
Daniele Fiaschè <fiaschedaniele@gmail.com>
2025-07-22 10:04:59 +0200

    cambio basi

---
+++ src/supabase_client.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url ="https://hqwgodduwropoldqavdt.supabase.co"
+const key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxd2dvZGR1d3JvcG9sZHFhdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwNjUwMDYsImV4cCI6MjA0NDY0MTAwNn0.-82Y5pZd_MPSG3n7PCAV7mAzRAHRnU-3-XzV-Wm8Lcc"
