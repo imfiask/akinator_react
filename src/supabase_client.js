396515495eb71e68eb351fff8262745f8ae1d2e5
Daniele Fiaschè <fiaschedaniele@gmail.com>
2025-10-01 15:57:19 +0200

    variabili d'ambiente

--- src/supabase_client.js
+++ src/supabase_client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 import { animeInGame } from "./Game"
 
-const url = "https://hqwgodduwropoldqavdt.supabase.co"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxd2dvZGR1d3JvcG9sZHFhdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwNjUwMDYsImV4cCI6MjA0NDY0MTAwNn0.-82Y5pZd_MPSG3n7PCAV7mAzRAHRnU-3-XzV-Wm8Lcc"
+const url = process.env.URL
+const key = process.env.KEY
 
 //prende tutti gli anime distinti
 export async function getAllAnime(){
